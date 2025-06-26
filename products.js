b1a3a5e882ddfb73deed85046f98e38c19826ebc
Kaynan Mwangi <kaynanmwangi6@gmail.com>
2025-06-26 20:13:27 +0300

    updates products.js

--- products.js
+++ products.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
       // Fetch Products
       async function fetchProducts() {
         try {
-          const response = await fetch('db.json');
+          const response = await fetch('./db.json');
           products = await response.json();
           renderProducts(products);
         } catch (error) {
