8d1acef26e09fc73e59abe8aef9364d0e8098ea9
Kaynan Mwangi <kaynanmwangi6@gmail.com>
2025-06-27 16:00:32 +0300

    updates files

--- navbarfiles/portfolio.js
+++ navbarfiles/portfolio.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
       imageUrl:
         "https://images.unsplash.com/photo-1579547945413-497e1b99dac0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
     },
-   
     {
       id: 3,
       title: "Personal Exploration",
